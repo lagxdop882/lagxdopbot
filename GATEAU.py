2944590c0098f223ef8baf940aed2da7eebbb6c6
lagxdop882 <155067717+lagxdop882@users.noreply.github.com>
2023-12-28 23:48:17 +0530

    Update GATEAU.py

--- GATEAU.py
+++ GATEAU.py
@@ -36,8 +36,8 @@ def Tele(cx):
 }
 
     data = {
-    'username': 'Zain55',
-    'password': 'Hamdi11@@',
+    'username': 'lagxdpp828383992929',
+    'password': 'LAGXDOP90',
     'woocommerce-login-nonce': login,
     '_wp_http_referer': '/my-account/',
     'login': 'Log in',
